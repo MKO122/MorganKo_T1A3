f8a311c6802ae26b21da7587cf931f37e6fede90
MKO122 <morgan.and.ko@hotmail.com>
2021-12-16 13:06:54 +1100

    Outlining Feature 1 and brainstorming

---
+++ brainstorm.rb
@@ -0,0 +1,11 @@
+puts "What is your name, Seeker?"
+name = gets.chomp.to_s
+
+puts "Hello " + name + "..."
+
+def 
+
+end
+
+#How do i 'gets' this information and store it in an array for later use...
+#Return to this after lunch
