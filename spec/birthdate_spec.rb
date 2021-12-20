784b5b096625ecb8dcd79afe87126b2a556cf846
MKO122 <morgan.and.ko@hotmail.com>
2021-12-20 23:56:25 +1100

    Deleted rspec files

--- spec/birthdate_spec.rb
+++
@@ -1,5 +0,0 @@
-describe 'birthdate' do
-    it 'rejects letters from entering the field' do
-        ?
-    end
-end
--- spec/yes_no_spec.rb
+++
@@ -1,5 +0,0 @@
-describe 'yes/no' do
-    it 'Confirmation accepts Y or N' do
-        ?
-    end
-end
